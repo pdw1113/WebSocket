5c734760528cd04b32ac05d84d86d9c565454cb5
SangNyu-Work <drnew0313@gmail.com>
2021-05-31 14:26:04 +0900

    log4jdbc dependency 추가

---
+++ src/main/java/nyu/socket/test/user/UserDTO.java
@@ -0,0 +1,26 @@
+package nyu.socket.test.user;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class UserDTO {
+	
+	private String userUuid;		// 고유번호
+	private String userName;		// 이름
+	private String userPassword;	// 비밀번호
+	private String userPic;			// 사진
+	
+}
